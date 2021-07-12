7edc8f981ae7c524a3561a572bbaface5ee9f527
Tazik <tshahjah@uwaterloo.ca>
2021-07-12 10:15:58 -0400

    Setup 101
    Add personal links.

--- site.config.js
+++ site.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '745999a4a0cc4a00ab140e39f4429ea5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Tash Talk',
+  domain: 'ta.sh',
+  author: 'Tazik Shahjahan',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Tazik Shahjahan TM',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  twitter: 'TazikShahjahan',
+  github: 'ZKTKZ',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
